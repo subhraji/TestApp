6babf2422a24fbc679a44ddc8efb8dac127a5807
subhrajit deb <subhrajitdeb54@gmail.com>
2020-11-27 17:11:26 +0530

    search view

--- app/src/main/java/com/example/testapp/views/activity/MainActivity.kt
+++ app/src/main/java/com/example/testapp/views/activity/MainActivity.kt
@@ -1,28 +1,76 @@
 package com.example.testapp.views.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
+import android.view.Menu
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuItemCompat
 import androidx.lifecycle.Observer
 import com.example.testapp.R
 import com.example.testapp.model.pojo.mars_property.MarsProperty
 import com.example.testapp.model.repos.Outcome
 import com.example.testapp.viewmodel.MarsViewModel
 import com.example.testapp.views.adapter.MarsPropertiesAdapter
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.activity_main.*
 import org.koin.android.viewmodel.ext.android.viewModel
+
 
 class MainActivity : AppCompatActivity() {
 
     private val marsViewModel: MarsViewModel by viewModel()
+    var listView: ListView? = null
+    var list: ArrayList<String>? = null
+    var adapter: ArrayAdapter<String>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        getMarsProperty()
+        //getMarsProperty()
+        listView = findViewById(R.id.listView)
+
+        list = ArrayList()
+        list!!.add("Apple")
+        list!!.add("Banana")
+        list!!.add("Pineapple")
+        list!!.add("Orange")
+        list!!.add("Lychee")
+        list!!.add("Gavava")
+        list!!.add("Peech")
+        list!!.add("Melon")
+        list!!.add("Watermelon")
+        list!!.add("Papaya")
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, list!!)
+        listView!!.setAdapter(adapter);
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search_menu, menu)
+
+       /* val searchItem = menu?.findItem(R.id.search_item)
+        val searchView = searchItem?.actionView as SearchView*/
+
+        val searchItem = menu?.findItem(R.id.search_item)
+        val searchView = searchItem?.actionView as SearchView
+        searchView.setQueryHint("Search View Hint")
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // task HERE
+                return false
+            }
+
+        })
+        return true
     }
 
     private fun getMarsProperty(){
